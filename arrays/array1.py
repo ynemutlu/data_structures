045db485dc4a4ee011a88e744acf1172fbff2a92
Yusuf Nemutlu <ymnemutlu@gmail.com>
2019-02-18 19:53:25 -0800

    cheat sheet for loops

--- arrays/array1.py
+++ arrays/array1.py
@@ -6,3 +6,17 @@ classroom = [student1,student2]
 for student in classroom:
     print("first name: {} ------------ last name: {}".format(student[0],student[1]))
  
+ 
+ 
+ # Loop over a single list with a regular for-in:
+
+# for n in numbers:
+    # print(n)
+# Loop over multiple lists at the same time with zip:
+
+# for header, rows in zip(headers, columns):
+    # print("{}: {}".format(header, ", ".join(rows)))
+# Loop over a list while keeping track of indexes with enumerate:
+
+# for num, line in enumerate(lines):
+    # print("{0:03d}: {}".format(num, line))
