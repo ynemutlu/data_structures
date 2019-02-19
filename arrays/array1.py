49dc6dcbdaa601d2277fdf6b708c3504ec4931a3
Yusuf Nemutlu <ymnemutlu@gmail.com>
2019-02-18 19:50:58 -0800

    first array test

---
+++ arrays/array1.py
@@ -0,0 +1,8 @@
+student1=["bilal","nemutlu","1155142"]
+student2=["burcu","boluknasi","1154970"]
+classroom = [student1,student2]
+ 
+
+for student in classroom:
+    print("first name: {} ------------ last name: {}".format(student[0],student[1]))
+ 
